9fcdeeba2c64c1cc208f2943a0d6cc1f05143bfd
Angela <chepkoechangela514@gmail.com>
2022-10-05 12:43:54 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -4,3 +4,19 @@ import Coin from './Coin';
 // import Navbar from './Navbar';
 // import Home from './Components/Home'; 
 import Button from 'react-bootstrap/Button';
+
+function App() {
+  const [coins,setCoins] = useState([])
+  const [search,setSearch] = useState('')
+  useEffect(() => {
+    fetch(' https://salty-depths-82016.herokuapp.com/crypto')
+    .then(res=> res.json())
+    .then(data =>setCoins(data))
+    .catch(error=>console.log(error))
+  }, [])
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+  const filteredCoins = coins.filter(coin=> coin.name.toLowerCase().includes(search.toLowerCase()
+    )
+    )
