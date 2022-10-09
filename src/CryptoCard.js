effcb7f46b00a40ce1f16b227f19d85407028b49
Angela <chepkoechangela514@gmail.com>
2022-10-09 23:46:27 +0300

    added coin prices

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -5,6 +5,9 @@ const CryptoCard = ({crypto}) => {
     <div className='cryptoCard'>
         <img src={crypto.image} alt='' className='cryptoImg' />
         <h1 className="cryptoName">{crypto.name}</h1>
+        <h4 className="cryptoPrice">
+            {crypto.price_change_percentage_24h}
+        </h4>
     </div>
   )
 }
