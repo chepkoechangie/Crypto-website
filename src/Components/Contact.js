551f2576d867c9248ca0d67bd79fa3b091c2de5c
Angela <chepkoechangela514@gmail.com>
2022-10-10 10:55:45 +0300

    listContainer final

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,23 @@
 import React from "react";
 function Contact(){
     return(
-        <div className="contact">
-           <h1>contact us via</h1>
-
+        <>
+           <h1 className="contactUs">contact us via</h1>
+           <div className="contact">
             <p>email:chepkoechangela514@gmail.com</p>
             <div>
                 <>Message: +254720511515</>
             </div>
+            <ul className="listContainer">
+                <li><a href="http://www.facebook.com">Facebook</a></li>
+                <li><a href="http://www.instagram.com">Instagram</a></li>
+                <li><a href="http://www.twitter.com">Twitter</a></li>
+                <li><a href="http://www.tiktok.com">Tiktok</a></li>
+            </ul>
+            
+                 </div>   
             <p></p>
-        </div>
+        </>
     )
 } 
 export default Contact;
